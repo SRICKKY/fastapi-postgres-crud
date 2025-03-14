fef29ca91d564525d6c4e2717594aaac8affad17
Srikant <kumar_srikant@outlook.com>
2025-03-14 14:43:34 +0530

    Initial FastAPI setup

---
+++ app/config.py
@@ -0,0 +1,14 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+
+    DB_URI: str
+
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+
+settings = Settings()
