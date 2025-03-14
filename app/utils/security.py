e68f2486922dada48a44a60f82768244497cde1c
Srikant <kumar_srikant@outlook.com>
2025-03-14 15:53:27 +0530

    utcnow to now()

--- app/utils/security.py
+++ app/utils/security.py
@@ -17,7 +17,7 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
     to_encode = data.copy()
-    expire = datetime.utcnow() + (
+    expire = datetime.now() + (
         expires_delta or timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     )
     to_encode.update({"exp": expire})
